ea1a187367e08de5a694a4b2a61511640aa9eb53
eduardomarcon <eduardommarcon@gmail.com>
2024-09-23 15:17:29 -0300

    feat: adding signup page

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,103 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signUpForm = z.object({
+  username: z.string(),
+  name: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      console.log(data)
+
+      toast.success('account created successfully', {
+        action: {
+          label: 'login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
+    } catch (error) {
+      toast.error('error creating account')
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Cadastro" />
+
+      <div className="p-8">
+        <Button variant="outline" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">login</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              crete your account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              have your finances under control
+            </p>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+            <div className="space-y-2">
+              <Label htmlFor="username">username</Label>
+              <Input id="username" type="text" {...register('username')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="name">name</Label>
+              <Input id="name" type="text" {...register('name')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="phone">phone</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              finish
+            </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              By continuing, you agree to our{' '}
+              <a href="" className="underline underline-offset-4">
+                terms of service
+              </a>{' '}
+              and{' '}
+              <a href="" className="underline underline-offset-4">
+                privacy policies
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
