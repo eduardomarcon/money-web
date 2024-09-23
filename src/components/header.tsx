6268fcac675440624b0c08ff47b8723911041bbd
eduardomarcon <eduardommarcon@gmail.com>
2024-09-23 16:47:47 -0300

    feat: adding header component

---
+++ src/components/header.tsx
@@ -0,0 +1,27 @@
+import { DollarSign, Home, Wallet } from 'lucide-react'
+
+import { NavLink } from '@/components/nav-link.tsx'
+import { Separator } from '@/components/ui/separator.tsx'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <DollarSign className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            home
+          </NavLink>
+          <NavLink to="/finances">
+            <Wallet className="h-4 w-4" />
+            finances
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  )
+}
